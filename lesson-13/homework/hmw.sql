005227afd6c0f6ed8f578f87de1c79cee2d1aec1
Yosinov <rizomuhammadyosinov@gmail.com>
2025-08-18 11:04:34 +0500

    Update hmw.sql

--- lesson-13/homework/hmw.sql
+++ lesson-13/homework/hmw.sql
@@ -1,23 +1,34 @@
 Easy tasks
 1 Select concat('100','-','Steven',' ','King')
+    
 2 Select replace(PHONE_NUMBER,'124','999')
     from Employees
-3 
+    
+3 SELECT FIRST_NAME, len(FIRST_NAME) as [Lenghts]
+    FROM Employees
+    WHERE (FIRST_NAME LIKE 'A%'
+    OR FIRST_NAME LIKE 'J%' 
+    OR FIRST_NAME LIKE 'M%')
+    
 4 select MANAGER_ID,
     Sum(SALARY) as Total_salary
     from Employees
   Group by MANAGER_ID
+    
 5 select Year1,
     greatest(Max1,Max2,Max3)
     from TestMax
+    
 6 select*from cinema
     where rating>'6.2'
     and id not like '4'
+    
 7 select*from SingleOrder
     order by
     case when id=0 then 1 else 0 end; 
-8
 
+8 
 
+    
 Medium Tasks
 1 
